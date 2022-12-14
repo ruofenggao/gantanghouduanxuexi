13921a7bcce72fcf05106d248374250d44441342
ruofengg <grfscott@hotmail.com>
2022-12-14 17:16:34 +0800

    first update

---
+++ 第一周/JavaStudy.java
@@ -0,0 +1,14 @@
+/* JavaStudy.java 12.13-12.16 2022
+ * Java	基本语法 基本数据类型包装类 面向对象 string类 集合框架
+ */
+public class JavaStudy {
+    //定义数据类型的关键字 class interface enum byte short int long float double char boolean void
+    //用于定义流程控制的关键字 if else switch case default for while do break continue return
+    //用于定义类与类之间的关系的关键字 extends implements
+    //用于定义异常处理的关键字 try catch finally throw throws
+    //用于定义访问权限修饰符的关键字 private protected public
+    //用于定义类，函数，变量修饰符的关键字 static final abstract synchronized
+    //用于定义类与类之间的关系的关键字 new instanceof this super
+    //其他修饰符 transient volatile strictfp native assert
+    //用于定义数据类型值的关键字 null true false
+}
